a25c1eb50f02a2f171987a33720996452ae2759a
vjgorla <vjgorla@gmail.com>
2017-05-05 19:15:15 +1000

    Update and rename src/main/test/java/bctest/vjgorla/github/com/BlockchainTest.java to src/test/java/bctest/vjgorla/github/com/BlockchainTest.java

--- src/test/java/bctest/vjgorla/github/com/BlockchainTest.java
+++ src/test/java/bctest/vjgorla/github/com/BlockchainTest.java
@@ -3,8 +3,6 @@ package bctest.vjgorla.github.com;
 import java.math.BigInteger;
 import java.util.List;
 
-import org.junit.Test;
-
 import bctest.vjgorla.github.com.Blockchain.Block;
 import junit.framework.TestCase;
 
@@ -16,7 +14,6 @@ public class BlockchainTest extends TestCase {
         blockchain = new Blockchain();
     }
     
-    @Test
     public void testIntialState() {
         assertEquals(blockchain.topBlockHash, Blockchain.ROOT_HASH);
         assertEquals(blockchain.topBlockNumber.intValue(), 0);
